8094e253e3957d03c26f9341b507bb7f4d2e3d57
Morgan Prior <morgan_prior@yahoo.co.uk>
2010-03-11 13:13:47 +0000

    *Added binary search with tests -Not Completed

---
+++ MeetUP/Session-03/binary_test.rb
@@ -0,0 +1,27 @@
+require 'test/unit'
+
+class BinSearchTest < Test::Unit::TestCase
+  def test_chop
+    assert_equal(-1, chop(3, []))
+    assert_equal(-1, chop(3, [1]))
+    assert_equal(0,  chop(1, [1]))
+
+    assert_equal(0,  chop(1, [1, 3, 5]))
+    assert_equal(1,  chop(3, [1, 3, 5]))
+    assert_equal(2,  chop(5, [1, 3, 5]))
+    assert_equal(-1, chop(0, [1, 3, 5]))
+    assert_equal(-1, chop(2, [1, 3, 5]))
+    assert_equal(-1, chop(4, [1, 3, 5]))
+    assert_equal(-1, chop(6, [1, 3, 5]))
+    
+    assert_equal(0,  chop(1, [1, 3, 5, 7]))
+    assert_equal(1,  chop(3, [1, 3, 5, 7]))
+    assert_equal(2,  chop(5, [1, 3, 5, 7]))
+    assert_equal(3,  chop(7, [1, 3, 5, 7]))
+    assert_equal(-1, chop(0, [1, 3, 5, 7]))
+    assert_equal(-1, chop(2, [1, 3, 5, 7]))
+    assert_equal(-1, chop(4, [1, 3, 5, 7]))
+    assert_equal(-1, chop(6, [1, 3, 5, 7]))
+    assert_equal(-1, chop(8, [1, 3, 5, 7]))
+  end
+end
