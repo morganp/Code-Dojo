2239dd84068545a85a7be6bf56d66031c64a5efe
morgy <morgy@Queeg500.local>
2010-03-11 23:59:09 +0000

    *Modified files to remove tab characters and uses spaces for indentation

--- ORM/lesson-01/solution/rakefile.rb
+++ ORM/lesson-01/solution/rakefile.rb
@@ -11,17 +11,17 @@ require 'active_record'
 #task :default => :db:migrate
 
 namespace :db do
-	desc "Migrate the database through scripts in db/migrate. Target specific version with VERSION=x"
-	task :migrate => :environment do
-		ActiveRecord::Migrator.migrate('migrate', ENV["VERSION"] ? ENV["VERSION"].to_i : nil )
-	end
-end	 
+   desc "Migrate the database through scripts in db/migrate. Target specific version with VERSION=x"
+   task :migrate => :environment do
+      ActiveRecord::Migrator.migrate('migrate', ENV["VERSION"] ? ENV["VERSION"].to_i : nil )
+   end
+end 
 
 task :environment do
    ActiveRecord::Base.establish_connection(
-		:adapter   => 'sqlite3',
-		:database  => './lesson1_AR.db'
-	)
+      :adapter   => 'sqlite3',
+      :database  => './lesson1_AR.db'
+   )
    ActiveRecord::Base.logger = Logger.new(File.open('database.log', 'a'))
 end
 
