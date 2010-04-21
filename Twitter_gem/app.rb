c784fe2da140e0d29f2e76de639b7f077807db19
Morgan Prior <morgan_prior@yahoo.co.uk>
2010-04-21 21:22:03 +0100

    updating main app.rb

--- Twitter_gem/app.rb
+++ Twitter_gem/app.rb
@@ -2,34 +2,26 @@
 require 'rubygems'
 require 'twitter'
 
-# Consumer Key
-cKey = "load from file"
-#
-# Consumer Secret
-cSecret = "load from file"
-#
-# Verified on site and recievied this pin
-# 8067944
+require 'consumer_keys'
+require 'access_token'
 
+load 'twitter_keys.rb'
 
-#Amarastech loged in and authorised the follwoing details
-# Request Tokens
-rToken = "Gk2b4HpxM8FFwHFCsTqIb3wyZqEB0hXXKHznzV7G0Y"
-rSecret = "77xp9TOkeNDhRXSereHHb7CmH81V8DoPxmTs0advs"
-pin =    0
-
-#access tokens
 
 # NOT SHOWN: granting access to twitter on website
 # and using request token to generate access token
-oauth = Twitter::OAuth.new(cKey, cSecret)
-oauth.authorize_from_request(rToken, rSecret, pin)
-oauth.authorize_from_access('access token', 'access secret')
+oauth = Twitter::OAuth.new(@twitter_keys[:consumer_key], @twitter_keys[:consumer_secret])
+
+#Requests valid for 1 API call, normally used to get access tokens
+#oauth.authorize_from_request(rToken, rSecret, pin)
+
+
+oauth.authorize_from_access(@twitter_keys[:access_token], @twitter_keys[:access_secret])
 
 
 client = Twitter::Base.new(oauth)
-client.friends_timeline.each  { |tweet| puts tweet.inspect }
-#client.user_timeline.each     { |tweet| puts tweet.inspect }
+#client.friends_timeline.each  { |tweet| puts tweet.inspect }
+#client.user_timeline.each     { |tweet| puts tweet.text }
 #client.replies.each           { |tweet| puts tweet.inspect }
 
 #client.update('Hello, http://amaras-tech.co.uk test tweet from ruby api')
